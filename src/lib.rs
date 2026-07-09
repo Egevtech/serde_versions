46abbc65ed6583d21253fe13f887b13f82aeaed2
Egevtech <egevtech@gmail.com>
2026-07-09 20:49:38 +0300

    Add test

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::fmt::Debug;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, PartialEq)]
 pub struct Version {
     major: u32,
     minor: u32,
@@ -22,5 +22,42 @@ impl Version {
             minor,
             patch,
         };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Version;
+    use serde::Deserialize;
+    use serde_json;
+
+    #[derive(Deserialize, Debug, PartialEq)]
+    struct Data {
+        version: Version,
+    }
+
+    #[test]
+    fn test() {
+        let json_str = r#"
+            {
+                "version": {
+                    "major": 1,
+                    "minor": 0,
+                    "patch": 0
+                }
+            }
+            "#;
+
+        let dt: Data = match serde_json::from_str(json_str) {
+            Ok(dt) => dt,
+            Err(err) => panic!("Failed to unwrap json: {}", err),
+        };
+
+        assert_eq!(
+            dt,
+            Data {
+                version: Version::new(1, 0, 0)
+            }
+        )
     }
 }
